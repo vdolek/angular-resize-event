1d91f69c4d3ba24db2d50321841df2a8c7afc427
Martin Volek <martin@vdolek.cz>
2018-01-13 01:03:36 +0100

    comment playground

--- playground/index.ts
+++ playground/index.ts
@@ -1,38 +1,38 @@
-/**
- * This is only for local test
- */
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { Component } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+// /**
+//  * This is only for local test
+//  */
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+// import { Component } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
-import { AngularResizedEventModule, ResizedEvent } from 'angular-resize-event';
+// import { AngularResizedEventModule, ResizedEvent } from 'angular-resize-event';
 
-@Component({
-  // tslint:disable-next-line:component-selector
-  selector: 'app',
-  styles: ['div { width: 50%; height: 50vh; size: 50%; font-size: 4em; background: red; resize: both; overflow: hidden; }'],
-  template: `
-  <div (resized)="onResized($event)">
-    {{ width }}x{{ height }}
-  </div>`
-})
-class AppComponent {
-  width: number;
-  height: number;
+// @Component({
+//   // tslint:disable-next-line:component-selector
+//   selector: 'app',
+//   styles: ['div { width: 50%; height: 50vh; size: 50%; font-size: 4em; background: red; resize: both; overflow: hidden; }'],
+//   template: `
+//   <div (resized)="onResized($event)">
+//     {{ width }}x{{ height }}
+//   </div>`
+// })
+// class AppComponent {
+//   width: number;
+//   height: number;
 
-  onResized(event: ResizedEvent): void {
-    this.width = event.newWidth;
-    this.height = event.newHeight;
-  }
-}
+//   onResized(event: ResizedEvent): void {
+//     this.width = event.newWidth;
+//     this.height = event.newHeight;
+//   }
+// }
 
-@NgModule({
-  bootstrap: [ AppComponent ],
-  declarations: [ AppComponent ],
-  imports: [ BrowserModule, AngularResizedEventModule ]
-})
-class AppModule {
-}
+// @NgModule({
+//   bootstrap: [ AppComponent ],
+//   declarations: [ AppComponent ],
+//   imports: [ BrowserModule, AngularResizedEventModule ]
+// })
+// class AppModule {
+// }
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+// platformBrowserDynamic().bootstrapModule(AppModule);
