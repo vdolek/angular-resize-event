e569c4f698bb53de8a94f9f5580976a5f8f73e55
Martin Volek <me@martinvolek.cz>
2020-08-31 19:56:26 +0200

    delete all

--- projects/angular-resize-event/src/lib/resized.directive.spec.ts
+++
@@ -1,9 +0,0 @@
-import { ElementRef } from '@angular/core';
-import { ResizedDirective } from './resized.directive';
-
-describe('ResizedDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ResizedDirective({} as ElementRef);
-    expect(directive).toBeTruthy();
-  });
-});
